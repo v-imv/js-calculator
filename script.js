d0e580a8f3ce09e613a8bf89cbb1c2f57c73665d
v-imv <v.iminov@gmail.com>
2022-10-14 11:15:42 +0100

    added arrow functions to operands to store clicked operand in a variable

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let displayValue = 0;
 let input1 = 0;
 let input2 = 0
+let operator = "";
 
+/*  */
 const calcDisplay = document.querySelector("div.display");
 const num7 = document.getElementById("7");
 const num8 = document.getElementById("8");
@@ -10,29 +12,28 @@ const del = document.getElementById("del");
 const num4 = document.getElementById("4");
 const num5 = document.getElementById("5");
 const num6 = document.getElementById("6");
-const plus = document.getElementById("plus");
 const num1 = document.getElementById("1");
 const num2 = document.getElementById("2");
 const num3 = document.getElementById("3");
-const minus = document.getElementById("minus");
 const period = document.getElementById("period");
 const num0 = document.getElementById("0");
-const divide = document.getElementById("divide");
-const times = document.getElementById("times");
 const reset = document.getElementById("reset");
 const equals = document.getElementById("equals");
+/*  */
 
-function addition(a, b){
-    return a + b;
-};
+/* Operands Start */
+const plus = document.getElementById("plus").addEventListener("click", () => operator = "plus");
+const minus = document.getElementById("minus").addEventListener("click", () => operator = "minus");
+const divide = document.getElementById("divide").addEventListener("click", () => operator = "divided_by");
+const times = document.getElementById("times").addEventListener("click", () => operator = "times");
+/* Operands End */
 
-function subtraction(a, b){
-    return a - b;
-};
+/* Functions Start */
+const addition = (a, b) => a + b;
 
-function multiplication(a, b){
-    return a * b;
-};
+const subtraction = (a, b) => a - b;
+
+const multiplication = (a, b) => a * b;
 
 function division(a, b){
     if(a !== 0 && b !== 0){
@@ -55,3 +56,9 @@ function calculate(a, operator, b){
         return division(a, b);
     };
 };
+/* Functions End  */
+
+
+// Manually Testing:
+// setInterval(print, 3000);
+// function print(){console.log(operator);};
