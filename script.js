2b082d1fcddb8c966b0a23162af3d69c971c667b
v-imv <v.iminov@gmail.com>
2022-10-18 21:36:42 +0100

    added logic to allow further operations on calculator results and behaviours for how calculator should function to use as a guide

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const calcDisplay = document.querySelector("div.display");
 
 /* Variable Storage Start */
 let result = 0;
-let input1 = 0;
-let input2 = 0;
+let input1 = undefined;
+let input2 = undefined;
 let operator = "+";
 let tempInputStorage = "";
 let inputTracker = 1;
@@ -25,11 +25,23 @@ const period = document.getElementById("period");
 
 /* Keypad Functionality Buttons Start */
 const equals = document.getElementById("equals").addEventListener("click", function(){
-    input2 = Number(tempInputStorage);
-    inputTracker = 1;
+    if(input2 == undefined){
+        input2 = Number(tempInputStorage);
+        inputTracker = 3;
+        result = calculate(input1, operator, input2);
+    }
+    else if(inputTracker == 3){
+        result = calculate(result, operator, Number(tempInputStorage));
+    };
     tempInputStorage = "";
-    result = calculate(input1, operator, input2);
     calcDisplay.innerHTML = result;
+
+    console.log(`equals run: 
+    tempStorage is ${tempInputStorage} 
+    input1 is ${input1} 
+    input2 is ${input2}
+    Tracker is ${inputTracker}
+    result is ${result}`);
 });
 
 const reset = document.getElementById("reset").addEventListener("click", function(){
@@ -92,26 +104,75 @@ function calculate(a, operator, b){
 
 function inputStorer(a){
     tempInputStorage = tempInputStorage.concat(a);
-    console.log(tempInputStorage + " " + tempInputStorage.length);
-    if(input1 == 0 && input2 == 0){
+    console.log(`
+    tempStorage is ${tempInputStorage} 
+    Storage length is ${tempInputStorage.length} 
+    input1 is ${input1} 
+    input2 is ${input2}
+    Tracker is ${inputTracker}
+    result is ${result}`);
+    if(input1 == undefined){
     calcDisplay.innerHTML = tempInputStorage;
     }
-    else if(input1 > 0){
+    else if(input2 == undefined){
         calcDisplay.innerHTML = `${input1} ${operator} ${tempInputStorage}`
+    }
+    else if(input2 !== undefined){
+        calcDisplay.innerHTML = `${result} ${operator} ${tempInputStorage}`
     };
 };
 
 function operatorLogic(){
-calcDisplay.innerHTML = tempInputStorage + " " + operator;
+// console.log(`run 1: tempStorage ${tempInputStorage} input1 ${input1} input2 ${input2}`)
     if(inputTracker == 1){
         input1 = Number(tempInputStorage);
         inputTracker += 1;
         tempInputStorage = "";
+        calcDisplay.innerHTML = `${input1} ${operator}`;
     }
     else if(inputTracker == 2){
         input2 = Number(tempInputStorage);
         inputTracker -= 1;
         tempInputStorage = "";
+    }
+    else if(inputTracker == 3){
+        calcDisplay.innerHTML = `${result} ${operator}`
     };
+console.log(`operator run: 
+tempStorage is ${tempInputStorage} 
+input1 is ${input1} 
+input2 is ${input2}
+inputTracker is ${inputTracker}
+result is ${result}`)
 };
 /* Calculator Functions End  */
+
+/* 
+Model Solution off of Calc Ya Later
+Intended Behaviour: 
+
+BEHAVIOUR 1
+10 + 10 = 20
+Press Equals Again = 20
+Press Equals Again = 20
+WORKS!
+
+BEHAVIOUR 2
+10 + 10 = 20
++ 5 = 25
+- 9 = 16
+WORKS!
+
+BEHAVIOUR 3
+10 + 10 = 20
+RESET
+10 + 10 = 20
+WIP
+*/
+
+console.log(`on start: 
+tempStorage is ${tempInputStorage} 
+input1 is ${input1} 
+input2 is ${input2}
+Tracker is ${inputTracker}
+result is ${result}`);
